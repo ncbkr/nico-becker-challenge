a3fb1e6356d72402ce8f098985b59163d415e432
Nico Becker <becker.nico.91@gmail.com>
2021-04-12 20:50:57 +0200

    implement app

--- src/app.py
+++ src/app.py
@@ -5,13 +5,26 @@ import numpy as np
 
 app = Flask(__name__)
 
+model = tf.keras.models.load_model('data/models/20210411_190339/model')
+
 @app.route('/inference', methods=["POST"])
 def inference():
     data = request.json
     img_arr = np.array(data["image"], dtype=np.uint8)
     # TODO: Replace with a call to your model
-    random_mask = (np.random.uniform(size=(img_arr.shape[:2])) > 0.5).astype(np.uint8)
-    return {"prediction": random_mask.tolist()}
+    print(img_arr.shape)
+    img_tensor = tf.convert_to_tensor(img_arr)
+    img_tensor = tf.image.resize(img_tensor, (128, 128))
+    img_tensor = tf.cast(img_tensor, tf.float32) / 255.0
+    #random_mask = (np.random.uniform(size=(img_arr.shape[:2])) > 0.5).astype(np.uint8)
+
+    prediction = model(img_tensor[tf.newaxis, ...])
+    prediction = prediction[0]
+
+    prediction = tf.image.resize(prediction, img_arr.shape[:2])
+    prediction = tf.squeeze(prediction)
+
+    return {"prediction": prediction.numpy().tolist()}
 
 if __name__ == '__main__':
     app.run(debug=True)
