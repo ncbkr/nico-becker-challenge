aaac1c7fa19d2a647a0fd38f3070c4cb57a34ec0
Nico Becker <becker.nico.91@gmail.com>
2021-04-18 12:00:38 +0200

    implement custom MeanIoU

---
+++ src/unet/metrics.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+from tensorflow.keras.metrics import MeanIoU
+
+class MeanIoU_Greater(MeanIoU):
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.greater(y_pred, 0.5)
+        return super().update_state(y_true, y_pred, sample_weight)
+        
+
+class MeanIoU_Argmax(MeanIoU):
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.argmax(y_pred, axis=-1)
+        y_pred = y_pred[..., tf.newaxis]
+
+        return super().update_state(y_true, y_pred, sample_weight)
+        
