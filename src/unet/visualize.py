cb941abec7c4d2e033800e66ff73b83664fcfd70
Nico Becker <becker.nico.91@gmail.com>
2021-04-18 12:00:05 +0200

    fix visualization of predicted masks

--- src/unet/visualize.py
+++ src/unet/visualize.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 
 
 def create_masks(pred_masks):
-    pred_masks = tf.argmax(pred_masks, axis=-1)
-    pred_masks = pred_masks[..., tf.newaxis]
+    if pred_masks.shape[-1] == 1:
+        pred_masks = tf.greater(pred_masks, 0.5)
+    else:
+        pred_masks = tf.argmax(pred_masks, axis=-1)
+        pred_masks = pred_masks[..., tf.newaxis]
     return pred_masks
 
 
@@ -44,10 +47,12 @@ def display(display_list):
 def show_prediction(model, images, targets):
     """assumes input batch"""
     pred_mask = model.predict(images)
+    pred_mask = create_masks(pred_mask)
     display([images[0], targets[0], pred_mask[0]])
 
 
 def export_prediction(model, images, targets, filename):
     """assumes input batch"""
     pred_mask = model.predict(images)
+    pred_mask = create_masks(pred_mask)
     export([images[0], targets[0], pred_mask[0]], filename)
